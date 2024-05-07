f6009c7b8b8d085ebca00c66591a8dd52dd19856
josetipula <flavio.tipula@utec.edu.pe>
2024-05-06 19:45:04 -0500

    so

--- BrailleConverter.py
+++ BrailleConverter.py
@@ -20,6 +20,7 @@ class BrailleConverter:
         """
 
         # TODO: Implement.
+        
         pass
 
     @staticmethod
--- PdfTextExtractor.py
+++ PdfTextExtractor.py
@@ -1,6 +1,6 @@
 from iSourceTextExtractor import iSourceTextExtractor
 from PyPDF2 import PdfReader
-
+import re
 class PdfTextExtractor(iSourceTextExtractor):
     """
     PdfTextExtractor
@@ -20,5 +20,7 @@ class PdfTextExtractor(iSourceTextExtractor):
         AllText = ""
         for PagePdf in range (len(self.pdf.pages)):
             Page = self.pdf.pages[PagePdf]
-            AllText += Page.extract_text()
+            TextWithLineBreaks = Page.extract_text() 
+            AllText += re.sub(r'\n',' ', TextWithLineBreaks)
+
         return AllText
